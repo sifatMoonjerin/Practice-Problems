0b5a432cfa832a32a192f88904e724d0a43895c2
sifatMoonjerin <smsrizon@gmail.com>
2020-01-31 23:34:58 +0600

    hasCycle() created

--- Linked List/Problem2.py
+++ Linked List/Problem2.py
@@ -10,3 +10,62 @@
 #          then there is no cycle in the linked list
 
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def insertAtTail(self, newNode, headNode):
+        if headNode is None:
+            self.head = newNode
+            return
+        
+        while headNode.next is not None:
+            headNode = headNode.next
+        
+        headNode.next = newNode
+
+
+    def hasCycle(self, head1, head2):
+        count = 1
+        while head1.next is not None:
+            head1 = head1.next
+            if (count%2) is 0: 
+                head2 = head2.next
+            if(head1 == head2):
+                print("Cycle present")
+                return
+            count += 1
+        
+        print("Cycle absent")
+
+
+    def printList(self, headNode):
+        while headNode is not None:
+            print(headNode.data)
+            headNode = headNode.next
+        
+
+linkedList = LinkedList()
+newNode = Node("asif")
+linkedList.insertAtTail(newNode, linkedList.head)
+cycleNode = Node("helal")
+linkedList.insertAtTail(cycleNode, linkedList.head)
+newNode = Node("anas")
+linkedList.insertAtTail(newNode, linkedList.head)
+newNode = Node("ashik")
+linkedList.insertAtTail(newNode, linkedList.head)
+newNode = Node("sabbir")
+linkedList.insertAtTail(newNode, linkedList.head)
+
+linkedList.insertAtTail(cycleNode, linkedList.head)
+
+
+
+linkedList.hasCycle(linkedList.head, linkedList.head)
