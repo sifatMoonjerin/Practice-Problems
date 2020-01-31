fc4e9c13ed18ac7dc5c3ced910bce341497958a4
sifatMoonjerin <smsrizon@gmail.com>
2020-01-31 20:33:51 +0600

    middleElement() implemented

--- Linked List/Problem1.py
+++ Linked List/Problem1.py
@@ -6,7 +6,7 @@
 #          iteration and the other head will enter the 
 #          next node at every two iteration. In this way,
 #          the second head will be at the middle when
-#          the first head reaches the end.
+#          the first head reaches the end. (ignoring empty list)
  
 
 class Node:
@@ -19,6 +19,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+
     def insertAtTail(self, newNode, headNode):
         if headNode is None:
             self.head = newNode
@@ -29,9 +30,31 @@ class LinkedList:
         
         headNode.next = newNode
 
+
+    def middleElement(self, head1, head2):
+        count = 1
+        while head1.next is not None:
+            head1 = head1.next
+            if (count%2) is 0: head2 = head2.next
+            count += 1
+        print(head2.data)
+
+
     def printList(self, headNode):
         while headNode is not None:
             print(headNode.data)
             headNode = headNode.next
         
 
+linkedList = LinkedList()
+
+
+#Enter list nodes...
+for i in range(5):
+    newNode = Node(input("Enter data: "))
+    linkedList.insertAtTail(newNode, linkedList.head)
+    
+
+linkedList.printList(linkedList.head)
+print("######################")
+linkedList.middleElement(linkedList.head, linkedList.head)
