ac9e5c29b0362d7a25ac00450e10d5602e4cfa0f
sifatMoonjerin <smsrizon@gmail.com>
2020-01-31 19:44:19 +0600

    approach stated

--- Linked List/Problem1.py
+++ Linked List/Problem1.py
@@ -1,6 +1,11 @@
 # Problem 1: How do you find the middle element 
 #            of a singly linked list in one pass?
 
-
+#Approach: Taking to pointers as head, I will iterate 
+#          both. One will enter the next node at every 
+#          iteration and the other head will enter the 
+#          next node at every two iteration. In this way,
+#          the second head will be at the middle when
+#          the first head reaches the end.
  
 
