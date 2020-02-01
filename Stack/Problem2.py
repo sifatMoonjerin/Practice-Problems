99fd0a0cafbd5554ddfe68fbe09ef8f679f0e448
sifatMoonjerin <smsrizon@gmail.com>
2020-02-01 23:29:48 +0600

    checked balaced parentheses

--- Stack/Problem2.py
+++ Stack/Problem2.py
@@ -19,8 +19,10 @@ class Stack:
 
 
     def stackPop(self):
-        if self.stackEmpty(): return 
+        if self.stackEmpty(): return False
+        data = self.top.data
         self.top = self.top.next
+        return data
 
 
     def stackTop(self):
@@ -31,3 +33,31 @@ class Stack:
         if self.top is None:
             return True
         return False
+
+
+stack = Stack()
+
+
+def balancedParentheses(str):
+    closePar = {
+        ']' : '[',
+        '}' : '{',
+        ')' : '('
+    }
+    for char in str:
+        if char == '[' or char == '{' or char == '(':
+            stack.stackPush(char)
+            continue
+
+        if char == ']' or char == '}' or char == ')':
+            check = stack.stackPop()
+            if check != closePar[char]: return False
+        
+    return stack.stackEmpty()
+
+
+
+string = input("Enter a test string: ")
+result = balancedParentheses(string)
+if result: print('Yap! Balanced Parentheses!')
+else: print('Nay!!!') 
