f0c005ed44acba105655b0d759b02a073e9dff86
sifatMoonjerin <smsrizon@gmail.com>
2020-02-02 17:37:15 +0600

    infix to postfix

---
+++ Problem3.py
@@ -0,0 +1,55 @@
+#Problem 3: Infix to Postfix
+
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+
+    def stackPush(self,data):
+        newNode = Node(data)
+        newNode.next = self.top
+        self.top = newNode
+
+
+    def stackPop(self):
+        if self.stackEmpty(): return False
+        data = self.top.data
+        self.top = self.top.next
+        return data
+
+
+    def stackTop(self):
+        if self.stackEmpty(): return False
+        return self.top.data
+            
+
+    def stackEmpty(self):
+        if self.top is None:
+            return True
+        return False
+
+operatorWeight = {
+    '+' : 1,
+    '-' : 1,
+    '*' : 2,
+    '/' : 2
+}
+
+openParentheses = {
+    ')' : '(',
+    '}' : '{',
+    ']' : '['
+}
+
+def isOperator(oper):
+    if oper == '+' or oper == '-' or oper == '*' or oper == '/':
+        return True
+    return False
