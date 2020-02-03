e2122bcdde0cd5786bd553bc4cee2ebec718e332
sifatMoonjerin <smsrizon@gmail.com>
2020-02-04 00:44:51 +0600

    insertData() created

--- Binary Search Tree/Problem1.py
+++ Binary Search Tree/Problem1.py
@@ -5,3 +5,33 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+
+    def insertData(self, data, tempRoot):
+        if self.root is None:
+            self.root = Node(data)
+            
+        else:
+            if tempRoot is None:
+                tempRoot = Node(data)
+            elif data > tempRoot.data:
+                tempRoot.right = self.insertData(data, tempRoot.right)
+            else:
+                tempRoot.left = self.insertData(data, tempRoot.left)
+            return tempRoot
+
+
+orchid = BST()
+orchid.insertData(15, orchid.root)
+orchid.insertData(10, orchid.root)
+orchid.insertData(20, orchid.root)
+orchid.insertData(25, orchid.root)
+orchid.insertData(8, orchid.root)
+orchid.insertData(12, orchid.root)
+
+
