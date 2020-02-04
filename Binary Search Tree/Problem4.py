13b106e0689901c587c4fe8fdf90e2ffcf4a45f4
sifatMoonjerin <smsrizon@gmail.com>
2020-02-05 00:24:28 +0600

    inSuc() started

--- Binary Search Tree/Problem4.py
+++ Binary Search Tree/Problem4.py
@@ -1,2 +1,44 @@
 #Inorder Successor
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+
+    def insertData(self, data, tempRoot):
+        if tempRoot is None:
+            self.root = Node(data)
+        elif data > tempRoot.data:
+            if tempRoot.right is None:
+                tempRoot.right = Node(data)
+            else:
+                self.insertData(data, tempRoot.right)
+        else:
+            if tempRoot.left is None:
+                tempRoot.left = Node(data)
+            else:
+                self.insertData(data, tempRoot.left)
+
+    
+    def inSuc(self):
+
+
+orchid = BST()
+
+orchid.insertData('F', orchid.root)
+orchid.insertData('D', orchid.root)
+orchid.insertData('J', orchid.root)
+orchid.insertData('B', orchid.root)
+orchid.insertData('A', orchid.root)
+orchid.insertData('E', orchid.root)
+orchid.insertData('K', orchid.root)
+orchid.insertData('G', orchid.root)
+orchid.insertData('C', orchid.root)
+orchid.insertData('I', orchid.root)
